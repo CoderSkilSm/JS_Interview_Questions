51010dde5dc99a9a332bfc9253927447f68a2e5e
CoderSkilSm <optimal844@gmail.com>
2024-10-27 07:52:14 +0530

    Create Map.js

---
+++ Map.js
@@ -0,0 +1,35 @@
+Map function example
+Approach - 1
+var numbers = [3,56,2,48,5]; 
+function double(x){
+    return x*2; 
+}
+newNumbers = numbers.map(double); 
+console.log(newNumbers);
+
+Approach - 2
+var numbers = [3,56,2,48,5]; 
+var newNumbers = []; 
+
+function double(x){
+    newNumbers.push(x*2); 
+}
+
+numbers.forEach(double); 
+console.log(newNumbers); 
+
+Approach - 3
+var numbers = [3,56,2,48,5]; 
+var newNumbers = numbers.map(function(x){
+    return x*2; 
+}); 
+console.log(newNumbers); 
+
+Approach - 4 
+var numbers = [3,56,2,48,5]; 
+var newNumbers = []; 
+numbers.forEach(function(x){
+    newNumbers.push(x*2); 
+});
+
+console.log(newNumbers);
